1a9779ce1db38bacf70f63168923ec652eba0d03
Gilang Raka Ramadhan <91415777+gilangdev@users.noreply.github.com>
2021-09-27 17:34:08 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
-const faders = document.querySelectorAll(".fade-in");
 
 hamburger.addEventListener("click", mobileMenu);
 function mobileMenu() {
@@ -14,26 +13,3 @@ function closeMenu() {
     navMenu.classList.remove("active");
 }
 
-const appearOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px -100px 0px";
-};
-const appearOnScroll = new IntersectionObserver
-(function(
-    entries, 
-    appearOnScroll
-) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-           return;   
-        } else {
-           entry.target.classList.add('appear');
-           appearOnScroll.unobserve(entry.target);
-        }
-    })
-}, 
-appearOptions); 
-
-faders.forEach(fader => {
-  appearOnScroll.observe(fader);  
-})
